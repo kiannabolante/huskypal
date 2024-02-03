991781e8007c31f999aef08d4de5dcaaa0ac260a
Vasu <vraguram@uw.edu>
2024-02-02 23:23:19 -0800

    passing github workflow testing!

--- clients/src/App.test.js
+++ clients/src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Closet component for /user/accessories route', () => {
+test('renders the Closet component for /user/accessories route', () => {
   render(<App />);
   const closetElement = screen.getByText(/Closet/i);
   expect(closetElement).toBeInTheDocument();
 });
 
-test('renders Tracker component for /user/level route', () => {
+test('renders the Tracker component for /user/level route', () => {
   render(<App />);
   const trackerElement = screen.getByText(/Tracker/i);
   expect(trackerElement).toBeInTheDocument();
