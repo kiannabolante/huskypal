05806209359981c998377d68887dc94cf5716b2c
Ramla <ramlaa@cs.washington.edu>
2024-02-13 00:56:07 -0800

    trackpal in progress

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TrackPal from "./components/TrackPal";
 import Closet from "./components/Closet";
 import Tracker from "./components/Tracker";
 import Homepage from "./components/Homepage";
@@ -10,28 +11,13 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/user/pal" element={<TrackPal />} />
           <Route path="/user/accessories" element={<Closet />} />
           <Route path="/user/level" element={<Tracker />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/user/todolist" element={<DubsTrack />} />
         </Routes>
       </Router>
-      {/* <a
-        className="Closet-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Closet
-      </a>
-      <a
-        className="Closet-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Tracker
-      </a> */}
     </div>
   );
 }
---
+++ clients/src/components/TrackPal.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import "./TrackPal.css";
+import { Link } from "react-router-dom";
+
+import deactivatedHome from "../images/nav-bar/deactivatedHome.png";
+import activatedPals from "../images/nav-bar/activatedPals.png";
+import deactivatedLevels from "../images/nav-bar/deactivatedLevels.png";
+import deactivatedCloset from "../images/nav-bar/deactivatedCloset.png";
+
+const emailNames = [
+    'ramlaa@uw.edu',
+    'kbolan@uw.edu',
+    'fatuma18@uw.edu',
+    'vraguram@uw.edu',
+    'fana21@uw.edu',
+  ];
+
+const TrackPal = () => {
+  return (
+    <div style={{ backgroundColor: "#F8F3E2" }}>
+      <h1> Track Pals </h1>
+      <section>
+        <h4> Connect with people completing the same track as you! </h4>
+        <hr />
+      </section>
+
+      {/* List of Pals */}
+      <div className="emailListContainer">
+        <ul className="emailList">
+          {emailNames.map((email, index) => (
+            <li key={index} className="emailItem">{email}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Navigation Bar */}
+      <section className="logos">
+        <div>
+          {" "}
+          <img
+            src={deactivatedHome}
+            alt="home button to get to the home page"
+            className="imageSize imageSpace"
+          />
+        </div>
+        <div>
+          <Link to="/user/accessories">
+            <img
+              src={deactivatedCloset}
+              alt="closet button to see accessories"
+              className="imageSize imageSpace"
+            />
+          </Link>
+        </div>
+        <div>
+          <Link to="/user/level">
+            <img
+              src={deactivatedLevels}
+              alt="levels button to see progress and rewards"
+              className="imageSize imageSpace"
+            />
+          </Link>
+        </div>
+        <div>
+          <Link to="/user/pal">
+            <img
+              src={activatedPals}
+              alt="pal button to connect with others"
+              className="imageSize imageSpace"
+            />
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default TrackPal;
